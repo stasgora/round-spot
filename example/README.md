9ca578991a435d42c09734216ed4f612b07af47d
Stanisław Góra <stasgora@gmail.com>
2021-04-09 18:40:27 +0200

    Example changes

--- example/README.md
+++ example/README.md
@@ -12,10 +12,14 @@
 
 ### Interacting with the application
 - tap the `First page` a few times
-- go to the `Second page` using a FAB
-- scroll the list, tap some items
+- go to the `Second page` using the button
+- scroll the list up and down
+- tap some of its items
 
 ### Getting the heat maps
 - exit the app or put it in background - do not kill it immediately to allow the heat maps to be generated
-- the heat maps are saved under a path returned by
+- heat maps are saved under a path returned by
 [`getApplicationDocumentsDirectory()`](https://pub.dev/documentation/path_provider/latest/path_provider/getApplicationDocumentsDirectory.html)
+- depending on your interactions the following files can be present:
+  - `first_.png` and `second_.png` containing those application pages
+  - `second_list.png` with the content of the list (entries from 0 to 49)
--- example/example.dart
+++ example/example.dart
@@ -5,45 +5,45 @@ import 'package:path_provider/path_provider.dart';
 import 'package:round_spot/round_spot.dart' as round_spot;
 
 void main() {
-	runApp(round_spot.initialize(
-		child: ExampleApp(),
-		config: round_spot.Config(
-			uiElementSize: 12,
-		),
-		heatMapCallback: (data, info) async {
-			var path = (await getApplicationDocumentsDirectory()).path;
-			File('$path/${info.page}_${info.area}.png').writeAsBytes(data);
-		},
-	));
+  runApp(round_spot.initialize(
+    child: ExampleApp(),
+    config: round_spot.Config(
+      uiElementSize: 12,
+    ),
+    heatMapCallback: (data, info) async {
+      var path = (await getApplicationDocumentsDirectory()).path;
+      File('$path/${info.page}_${info.area}.png').writeAsBytes(data);
+    },
+  ));
 }
 
 class ExampleApp extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			title: 'Example Application',
-			navigatorObservers: [round_spot.Observer()],
-			initialRoute: 'first',
-			routes: {
-				'first': (context) => Scaffold(
-					appBar: AppBar(title: Text('First page')),
-					floatingActionButton: FloatingActionButton(
-						child: Icon(Icons.arrow_forward),
-						onPressed: () => Navigator.pushNamed(context, 'second'),
-					),
-				),
-				'second': (context) => Scaffold(
-					body: SingleChildScrollView(
-						child: round_spot.ListDetector(
-							areaID: 'list',
-							children: [
-								for (int i = 0; i < 50; i++) ListTile(title: Text('$i'))
-							],
-						),
-					),
-					appBar: AppBar(title: Text('Second page')),
-				)
-			},
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Example Application',
+      navigatorObservers: [round_spot.Observer()],
+      initialRoute: 'first',
+      routes: {
+        'first': (context) => Scaffold(
+              appBar: AppBar(title: Text('First page')),
+              floatingActionButton: FloatingActionButton(
+                child: Icon(Icons.arrow_forward),
+                onPressed: () => Navigator.pushNamed(context, 'second'),
+              ),
+            ),
+        'second': (context) => Scaffold(
+              body: SingleChildScrollView(
+                child: round_spot.ListDetector(
+                  areaID: 'list',
+                  children: [
+                    for (int i = 0; i < 50; i++) ListTile(title: Text('$i'))
+                  ],
+                ),
+              ),
+              appBar: AppBar(title: Text('Second page')),
+            ),
+      },
+    );
+  }
 }
